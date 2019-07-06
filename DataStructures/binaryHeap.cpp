a422192890ad2b76843e5fcdfc27323e4e85d161
RajibTheKing <aporba.das@gmail.com>
2019-07-06 16:07:03 +0200

    fix bug in binaryHeap implementation

--- DataStructures/binaryHeap.cpp
+++ DataStructures/binaryHeap.cpp
@@ -2,11 +2,9 @@
 #include <cmath>
 using namespace std;
 
-int heapSize = 0;
-
-void ShowArray(int A[], int len)
+void ShowArray(int A[], int n)
 {
-    for(int i=0; i<len; i++)
+    for(int i=1; i<=n; i++)
     {
         cout<<A[i]<<" ";
     }
@@ -26,17 +24,17 @@ int RIGHT(int i)
     return 2 * i + 1;
 }
 
-void MAX_HEAPIFY(int A[], int indx)
+void MAX_HEAPIFY(int A[], int maxIndex, int indx)
 {
     int l = LEFT(indx);
     int r = RIGHT(indx);
     int largest;
-    if (l < heapSize && A[l] > A[indx])
+    if (l <= maxIndex && A[l] > A[indx])
         largest = l;
     else
         largest = indx;
 
-    if(r < heapSize && A[r] > A[largest])
+    if(r <= maxIndex && A[r] > A[largest])
         largest = r;
     
     if (largest != indx)
@@ -44,53 +42,53 @@ void MAX_HEAPIFY(int A[], int indx)
         int temp = A[indx];
         A[indx] = A[largest];
         A[largest] = temp;
-        MAX_HEAPIFY(A, largest);
+        MAX_HEAPIFY(A, maxIndex, largest);
     }
 }
 
 
-void BUILD_MAX_HEAP(int A[], int len)
+void BUILD_MAX_HEAP(int A[], int maxIndex)
 {
-    heapSize = len;
-    for(int i = len - 1; i>=0; i--)
+    for(int i = maxIndex; i>=1; i--)
     {
-        MAX_HEAPIFY(A, i);
+        MAX_HEAPIFY(A, maxIndex, i);
     }
+    
 }
 
-void HEAP_SORT(int A[], int len)
+void HEAP_SORT(int A[], int maxIndex)
 {
-    BUILD_MAX_HEAP(A, len);
-    heapSize = len;
-    for(int i=len-1; i>=0; i--)
+    BUILD_MAX_HEAP(A, maxIndex);
+    for(int i=maxIndex; i>=1; i--)
     {
-        int temp = A[0];
-        A[0] = A[i];
+        int temp = A[1];
+        A[1] = A[i];
         A[i] = temp;
-        heapSize = heapSize - 1;
-        MAX_HEAPIFY(A, 0);
+        maxIndex = maxIndex - 1;
+        MAX_HEAPIFY(A, maxIndex, 1);
     }
-    ShowArray(A, len);
+    ShowArray(A, maxIndex);
 }
 
-int HEAP_EXTRACT_MAX(int A[])
+int HEAP_EXTRACT_MAX(int A[], int &maxIndex)
 {
-    if (heapSize <= 0)
+    ShowArray(A, maxIndex);
+    if (maxIndex <= 0)
     {
         cout<<"No values"<<endl;
         return INT32_MIN;
     }
-    int res = A[0];
-    A[0] = A[heapSize-1];
-    heapSize--;
-    MAX_HEAPIFY(A, 0);
+    int res = A[1];
+    A[1] = A[maxIndex];
+    maxIndex--;
+    MAX_HEAPIFY(A, maxIndex, 1);
     return res;
 }
 
 void HEAP_INCREASE_KEY(int A[],  int indx, int  key)
 {
     A[indx] = key;
-    while(indx > 0 && A[PARENT(indx)] < A[indx])
+    while(indx > 1 && A[PARENT(indx)] < A[indx])
     {
         int temp = A[indx];
         A[indx] = A[PARENT(indx)];
@@ -103,10 +101,17 @@ int main()
 {
     int arr[] = {34, 12, 76, 35, 90, 112, 56, 60, 99};
     int len = 9;
-    BUILD_MAX_HEAP(arr, len);
-    for(int i=0; i<len;i++)
+    //to implement Binary heap, I am considering 1 indexed array
+    //Benifit: parent child calculation will be bugfree
+    int arrOneIndexed[len+1];
+    int maxIndex = len;
+    for(int i=1; i<=maxIndex; i++)
+        arrOneIndexed[i] = arr[i-1];
+    
+    BUILD_MAX_HEAP(arrOneIndexed, len);
+    while(maxIndex)
     {
-        cout<<HEAP_EXTRACT_MAX(arr)<<endl;
+        cout<<HEAP_EXTRACT_MAX(arrOneIndexed, maxIndex)<<endl;
     }
    
     return 0;
