2a9f09afe35b23f9312fd6060dfc965f86be15fd
RajibTheKing <aporba.das@gmail.com>
2019-07-08 22:04:27 +0200

    modify.. little bit

--- SortingInLinearTime/kthLargest.cpp
+++ SortingInLinearTime/kthLargest.cpp
@@ -27,26 +27,16 @@ int partition(int arr[], int l, int r)
 // This function returns k'th smallest element in arr[l..r] using 
 // QuickSort based method.  ASSUMPTION: ALL ELEMENTS IN ARR[] ARE DISTINCT 
 int kthLargest(int arr[], int l, int r, int k) 
-{ 
-    // If k is smaller than number of elements in array 
-    if (k > 0 && k <= r - l + 1) 
-    { 
-        // Partition the array around last element and get 
-        // position of pivot element in sorted array 
+{  
         int pos = partition(arr, l, r); 
-  
         // If position is same as k 
-        if (pos-l == k-1) 
+        if (pos == k-1) 
             return arr[pos]; 
-        if (pos-l > k-1)  // If position is more, recur for left subarray 
+        if (pos > k-1)  // If position is more, recur for left subarray 
             return kthLargest(arr, l, pos-1, k); 
   
         // Else recur for right subarray 
-        return kthLargest(arr, pos+1, r, k-pos+l-1); 
-    } 
-  
-    // If k is more than number of elements in array 
-    return INT_MAX; 
+        return kthLargest(arr, pos+1, r, k); 
 } 
 
 
@@ -55,7 +45,7 @@ int main()
     //nlogn 
     //O(n)
     int arr[] = {12, 3, 5, 7, 4, 19, 26}; 
-    int n = sizeof(arr)/sizeof(arr[0]), k = 3; 
-    cout << "K'th smallest element is " << kthLargest(arr, 0, n-1, k) <<endl; 
+    int n = sizeof(arr)/sizeof(arr[0]), k = 5; 
+    cout << "K'th largest element is " << kthLargest(arr, 0, n-1, k) <<endl; 
     return 0; 
 } 
--- ToyProblem/maximumSubarray.cpp
+++ ToyProblem/maximumSubarray.cpp
@@ -7,16 +7,15 @@ using namespace std;
 
 int maxSubArraySum(int a[], int size)
 { 
-	int max_so_far = a[0], max_ending_here = 0; 
+	int max_so_far = a[0], max_ending_here = a[0];
+	
+	
 
-	for (int i = 0; i < size; i++) 
+	for (int i = 1; i < size; i++) 
 	{ 
-		max_ending_here = max_ending_here + a[i]; 
-		if (max_so_far < max_ending_here) 
-			max_so_far = max_ending_here; 
-
-		if (max_ending_here < 0) 
-			max_ending_here = 0; 
+		max_ending_here = max(a[i], max_ending_here + a[i]); 
+		max_so_far = max(max_so_far, max_ending_here); 
+		
 	} 
 	return max_so_far; 
 } 
